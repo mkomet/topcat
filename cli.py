1d69a33c94c04872f4034b6d27b4251d0d2a2da8
uriariel <uri.ariel@granulate.io>
2022-06-10 03:57:31 +0300

    Add more granulated hourly search

--- cli.py
+++ cli.py
@@ -1,20 +1,23 @@
 import datetime
+
 import fire
 
-from topcat import VenueAvailability, search_availability
+from topcat import search_availability
 from topcat.request import BookingMethod
+
 
 def cli(restaurant: str, start_date_str: str, end_date_str: str):
     start_date = datetime.datetime.strptime(start_date_str, '%d/%m/%y-%H:%M')
     end_date = datetime.datetime.strptime(end_date_str, '%d/%m/%y-%H:%M')
-    date_generated = [start_date + datetime.timedelta(days=x) for x in range(0, (end_date-start_date).days)]
+    date_generated = [start_date + datetime.timedelta(hours=x) for x in
+                      range(0, int((end_date - start_date).total_seconds() // 3600), 2)]
 
     for date in date_generated:
         availability = search_availability(2, date, restaurant)
         for area in availability.areas or []:
             for option in area.options or []:
-                if (option.method == BookingMethod.SEAT):
-                    print(f"date: {date} time: {option.time}")
+                if option.method == BookingMethod.SEAT:
+                    print(f"Date: {date} Time: {option.time}")
 
 
 if __name__ == "__main__":

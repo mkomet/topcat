c5e4c1f0dbb9d868d964c7ba4febcc52435b1f11
uri ariel <uri.ariel@granulate.io>
2022-08-14 23:13:52 +0300

    Fix ontopo api

---
+++ topcat/const.py
@@ -0,0 +1,7 @@
+ONTOPO_URL = "https://ontopo.co.il"
+ONTOPO_AVAILABILITY_API = f"{ONTOPO_URL}/api/availability/searchAvailability"
+ONTOPO_SEARCH_VENUE_API = f"{ONTOPO_URL}/api/search/searchVenues"
+ONTOPO_FETCH_VENUE_API =  f"{ONTOPO_URL}/api/search/fetchVenueProfile"
+
+EN_US_LOCALE = "en-us"
+SEARCH_VENUE_SLUG = "15171493"
